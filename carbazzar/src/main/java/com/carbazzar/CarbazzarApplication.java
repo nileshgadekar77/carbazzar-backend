32fb02b95ec9d92ac165f5367ff7055ec4ed766e
Nilesh <nileshgadekar77@gmail.com>
2025-09-23 16:20:55 +0530

    Initial commit - Spring Boot backend

---
+++ carbazzar/src/main/java/com/carbazzar/CarbazzarApplication.java
@@ -0,0 +1,13 @@
+package com.carbazzar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarbazzarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarbazzarApplication.class, args);
+	}
+
+}
---
+++ carbazzar/src/test/java/com/carbazzar/CarbazzarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carbazzar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarbazzarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
